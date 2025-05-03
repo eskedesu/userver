333cbd45866a794c8acb05fb4e9201c25e53baa9
ezzkemer <ezzkemer@yandex.ru>
2025-05-03 19:33:48 +0300

    fix doc

--- etcd/include/userver/etcd/component.hpp
+++ etcd/include/userver/etcd/component.hpp
@@ -19,6 +19,14 @@ namespace etcd {
 ///
 /// Provides access to a etcd cluster.
 ///
+/// ## Static options:
+/// Name                               | Description                                                | Default value
+/// ---------------------------------- | ---------------------------------------------------------- | ---------------
+/// endpoints                          | Etcd endpoints to which client make HTTP requests          | -
+/// attempts                           | Number of attempts to each endpoint, on failed attempts client randomly moves to another endpoint | 3
+/// request_timeout_ms                 | Timeout for all HTTP requests to etcd except watch request | 1000
+/// watch_timeout_ms                   | Timeout for watch HTTP request. It's a stremed request, so it is used also as a connection timeout, so it should not be too short | 1000000
+///
 // clang-format on
 
 class Component final : public components::ComponentBase {
