77662efdc0d6259de15c5d8be417fbedba230888
ezzkemer <ezzkemer@yandex.ru>
2025-05-16 03:20:36 +0300

    fix convertion

--- etcd/src/etcd/client_impl.cpp
+++ etcd/src/etcd/client_impl.cpp
@@ -41,8 +41,7 @@ const std::string kKeyPrefix = "/etcd/";
 
 KeyValueState ConvertRawKeyValueState(const etcd_schemas::RawKeyValueState& raw_key_value_state) {
     KeyValueState key_value_state;
-    key_value_state.key = raw_key_value_state.key.GetUnderlying()
-                              .substr(kKeyPrefix.size());
+    key_value_state.key = raw_key_value_state.key.GetUnderlying().substr(kKeyPrefix.size());
     key_value_state.value = raw_key_value_state.value.GetUnderlying();
     key_value_state.version = std::stoi(raw_key_value_state.version);
     return key_value_state;
